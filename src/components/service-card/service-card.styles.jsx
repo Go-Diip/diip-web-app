d81953509a50bff1a5783164a677013ed776c80d
Alex Ron <bryan.ron@hotmail.com>
2022-07-18 23:16:22 -0500

    feat: add service card

--- src/components/service-card/service-card.styles.jsx
+++ src/components/service-card/service-card.styles.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import CustomImage from "../custom-image/custom-image.component"
+import { Typography } from "@mui/material"
 
 export const Wrapper = styled.div`
   width: 100%;
@@ -18,5 +19,22 @@ export const Image = styled(CustomImage)`
     width: 100%;
     height: 100%;
     object-fit: cover !important;
+  }
+`
+
+export const Title = styled.h2`
+  margin-bottom: ${({ theme }) => theme.typography.pxToRem(56)};
+  ${({ theme }) => theme.breakpoints.up("md")} {
+    font-weight: 600;
+    font-size: ${({ theme }) => theme.typography.pxToRem(40)};
+    line-height: 48px;
+  }
+`
+
+export const Description = styled(Typography)`
+  ${({ theme }) => theme.breakpoints.up("md")} {
+    font-weight: 500;
+    font-size: ${({ theme }) => theme.typography.pxToRem(18)};
+    line-height: 28px;
   }
 `
